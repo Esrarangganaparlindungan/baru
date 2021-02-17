f5db8359e04fca2becba99b390b844884b04dda4
Esrarangganaparlindungan <78409072+Esrarangganaparlindungan@users.noreply.github.com>
2021-02-17 16:57:15 +0700

    penugasan

---
+++ penugasan.js
@@ -0,0 +1,21 @@
+document.write("Mula-mula nilai score...<br>");
+var score = 100;
+document.write("score = "+ score + "<br/>");
+
+score += 4;
+document.write("score = "+ score + "<br/>");
+
+score -= 1;
+document.write("score = "+ score + "<br/>");
+
+score *= 3;
+document.write("score = "+ score + "<br/>");
+
+score /= 6;
+document.write("score = "+ score + "<br/>");
+
+score **= 1;
+document.write("score = "+ score + "<br/>");
+
+score %= 5;
+document.write("score = "+ score + "<br/>");
