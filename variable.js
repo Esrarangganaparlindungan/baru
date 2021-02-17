49683ce41475f31ddbc79bc458ad70d3bd27ba4f
Esrarangganaparlindungan <78409072+Esrarangganaparlindungan@users.noreply.github.com>
2021-02-17 16:56:41 +0700

    variable

---
+++ variable.js
@@ -0,0 +1,10 @@
+var name =("Esra ranggana parlindungan");
+var umur = 15;
+var tanggal = 10;
+
+document.write("nama :" + name + "<br>");
+document.write("umur :" + umur + " <br>");
+document.write("tanggal lahir :" +tanggal + " <br>");
+
+alert("selamat datang esra");
+
