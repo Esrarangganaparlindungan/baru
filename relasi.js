0762c18ca91e77503a81f951313f294aebf9cecb
Esrarangganaparlindungan <78409072+Esrarangganaparlindungan@users.noreply.github.com>
2021-02-17 16:57:01 +0700

    relasi

---
+++ relasi.js
@@ -0,0 +1,22 @@
+var siapa = 12;
+var aku = 14;
+
+var hasil = siapa == aku
+document.write(`${siapa}==${aku} = ${hasil} <br//>`);
+ 
+var hasil = siapa > aku;
+document.write(`${siapa} > ${aku} = ${hasil} <br/>`);
+
+var hasil = siapa >= aku;
+document.write(`${siapa} >= ${aku} = ${hasil} <br/>`);
+
+var hasil = siapa < aku;
+document.write(`${siapa} < ${aku} = ${hasil} <br/>`);
+
+var hasil = siapa <= aku;
+document.write(`${siapa} <= ${aku} = ${hasil} <br/>`);
+
+var hasil = siapa != aku;
+document.write(`${siapa} != ${siapa} = ${hasil} <br/>`);
+
+var hasil = siapa 
