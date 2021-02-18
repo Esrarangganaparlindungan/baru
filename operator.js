0532c18f1d3daa4aa744161dc3ab99fb784d40fd
Esrarangganaparlindungan <78409072+Esrarangganaparlindungan@users.noreply.github.com>
2021-02-18 16:33:15 +0700

    operator.js

---
+++ operator.js
@@ -0,0 +1,26 @@
+var a=3;
+var b=4;
+
+var hasil = a & b;
+document.write(`${a} & ${b} = ${hasil} <br/>`);
+
+var hasil = a | b;
+document.write(`${a} | ${b} = ${hasil} <br/>`);
+
+var hasil = a ^ b;
+document.write(`${a} ^ ${b} = ${hasil} <br/>`);
+
+var hasil = ~a;
+document.write(`~${a} = ${hasil}<br/>`);
+        
+
+var hasil = a >> b;
+document.write(`${a} >> ${b} = ${hasil}<br/>`);
+
+
+var hasil = a << b;
+document.write(`${a} << ${b} = ${hasil}<br/>`);
+
+
+var hasil = a >>> b;
+document.write(`${a} >>> ${b} = ${hasil}<br/>`);
